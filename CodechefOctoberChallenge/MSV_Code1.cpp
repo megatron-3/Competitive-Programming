189ae7ad5b1c72283b5636785d301e0a58f779d0
megatron-3 <36893208+megatron-3@users.noreply.github.com>
2019-10-08 03:38:43 +0530

    Update MSV_Code1.cpp

--- CodechefOctoberChallenge/MSV_Code1.cpp
+++ CodechefOctoberChallenge/MSV_Code1.cpp
@@ -1,64 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <utility>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
-
-bool compare (pair <int, int> p1, pair <int, int> p2) {
-    return p1.first < p2.first;
-}
 
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, tmp;
+        int n;
         cin >> n;
+        vector <int> vec(n);
 
-        vector <pair <int, int> > vec(n);
         for (int i=0;i<n;i++) {
-            cin >> tmp;
-            vec[i] = make_pair (tmp, 0);
+            cin >> vec[i];
         }
 
-        for (int i=n-1;i>=0;i--) {
-            if (vec[i].second != 0) {
-                if (vec[i].second == -1) {
-                    vec[i].second = 0;
-                }
-                continue;
+        vector <int> num (1000005, 0);
+        int maxi = 0; 
+        for (int i=0;i<n;i++) {
+            if (maxi < num[vec[i]]) {
+                maxi = num[vec[i]];
             }
-
-            vector <int> temp(n, 0);
-            int count = 0;
-            for (int j=i-1;j>=0;j--) {
-                if (vec[j].first % vec[i].first == 0) {
-                    temp[count] = j;
-                    vec[j].second = -1;
-                    count++;
-                    vec[i].second++;
-
-                    for (int k=0;k<count-1;k++) {
-                        if (vec[j].first % vec[temp[k]].first == 0) {
-                            if (vec[temp[k]].second == -1) {
-                                vec[temp[k]].second++;
-                            }
-                            vec[temp[k]].second++;
-                        }
+            for (int j=1;j<=sqrt (vec[i]);j++) {
+                if (vec[i]%j == 0) {
+                    if (vec[i] / j == j) {
+                        num[j]++;
+                    }
+                    else {
+                        num[vec[i] / j]++;
+                        num[j]++;
                     }
                 }
             }
         }
 
-        int highest = 0;
-        for (int i=0;i<n;i++) {
-            if (highest < vec[i].second) {
-                highest = vec[i].second;
-            }
-        }
-
-        cout << highest << "\n";
+        cout << maxi << "\n";
     }
     return 0;
 }
